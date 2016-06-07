4d1ad9ad70fa1f781d1ef0c1d5cb99a9ffb1efec
Ching Hsu <martin.hsuching@gmail.com>
2016-06-07 12:01:25 +0800

    add Faker

--- Library/String.js
+++ Library/String.js
@@ -47,3 +47,22 @@
     // return str.substr(halfIndex).reverse() + str.substr(0, halfIndex).reverse();
 
 });
+
+
+!String.prototype.hasOwnProperty("palindrome") && (String.prototype.palindrome = function () {
+//     \W删除所有非常字母数字字符：
+//
+//     \W匹配一个非单字字符
+//     等价于[^A-Za-z0-9_]
+//     那么\W的意思就是：
+//
+// [^A-Z]匹配非26个大写字母中的任意一个
+//         [^a-z]匹配非26个小写字母中的任意一个
+//         [^0-9]匹配非0到9中的任意一个数字
+//         [^_]匹配非下划线
+
+    return this.replace(/[\W_]/g, '').toLowerCase() ===
+        this.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+
+
+});
