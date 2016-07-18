3e9fa770ede1c0f509c144e3efd4764036d9b1b4
Ching Hsu <martin.hsuching@gmail.com>
2016-07-18 18:47:37 +0800

    fix

--- Infinite/js/infinitelist.js
+++ Infinite/js/infinitelist.js
@@ -2,15 +2,26 @@
  * Created by Hsuching on 16/7/17.
  */
 var cursor = {
-    current: 0,
-    position: []
+    //滑动单元起始终止位置
+    current: {
+        start: 0,
+        end: -1
+    },
+    //每个数据单元的跨度
+    position: [],
+    //滑动位置
+    scrollPosition: {
+        y:0,
+        x:0
+    }
 };
 var number = 50;
 var sectionEl = document.querySelector(".section");
 var initialPosition = {
-    x: sectionEl.offsetWdith,
+    x: sectionEl.offsetWidth,
     y: sectionEl.offsetHeight
 };
+
 
 for (var i = 0; i < number; i++) {
     var elFg = document.createElement("figure");
@@ -29,24 +40,59 @@ for (var k = 1; k <= 1000; k++) {
 
 var els = sectionEl.querySelectorAll(".listItem");
 
-setTimeout(loadData, 100);
-function loadData() {
-    for (var j = 0; j < 10; j++) {
+function calculate() {
+
+    return 20;
+}
+
+function calItemsIndex(index, num) {
+    return index >= 0 ? (index % num) : ((num - 1) - (~index % num));
+}
+
+
+setTimeout(loadData(calculate()), 100);
+
+
+function loadData(num, param) {
+    var fillItemsNumber = num;
+    for (var j = 0; j < fillItemsNumber; j++) {
         var lastTop = 0;
-        if (cursor.current) {
+        if (cursor.current.end != -1) {
             lastTop = cursor.position.reduce(function (a, b) {
                 return a + b;
             });
         }
-        var el = els[j];
-        el.innerHTML = data[j];
+        cursor.current.end++;
+        var el = els[cursor.current.end % number];
+        el.classList.add("active");
+        el.innerHTML = data[cursor.current.end];
         el.style.transitionDelay = (j) * 0.3 + "s";
         el.style.top = lastTop + "px";
-        cursor.position.push(els[cursor.current].offsetHeight);
-        cursor.current++;
+        cursor.position.push(els[cursor.current.end % number].offsetHeight);
     }
 }
 
+function isDataThreshold(scroll, cursor) {
+    var fillItemsNumber = calculate();
+    var topThreshold = 0;
+    var height = cursor.position.reduce(function (a, b) {
+        return a + b;
+    });
+    cursor.position.reduce(function (a, b) {
+        if (a >= 0.7 * height) {
+            topThreshold = a + b;
+            return false;
+        } else {
+            return a + b;
+        }
+    });
+    if ((scroll.offset.y < (-1) * topThreshold)) {
+        return true
+    } else {
+        return false;
+    }
+
+}
 
 function splitStr(str) {
     return str.trim().split(/\s+/g);
@@ -125,11 +171,14 @@ function _update() {
             diff = (y - startY);
             startY = y;
             direction = (diff <= 0) ? 1 : -1;
-            currentPosition.y += diff;
+            cursor.scrollPosition.y+=diff;
             var param = {
-                diff: currentPosition.y
+                offset: {
+                    y: cursor.scrollPosition.y
+                }
             };
-            _scrollRefreshData(scrollSection.children, param);
+            refresh(param);
+            _scroll(scrollSection.querySelectorAll("*>figure.active"), param);
 
         };
         addEventListeners(scrollMask, "mousemove touchmove", scrollMoveCallback);
@@ -140,11 +189,16 @@ function _update() {
         e.stopPropagation();
         removeEventListeners(scrollMask, "mousemove touchmove", scrollMoveCallback);
         var param = {};
-        if (currentPosition.y >= 0) {
-            param.diff = 0;
-            currentPosition.y = 0;
+        if (cursor.scrollPosition.y >= 0) {
+            cursor.scrollPosition.y = 0;
+            param = {
+                offset: {
+                    y: cursor.scrollPosition.y
+                }
+            };
             param.back = true;
-            _scrollRefreshData(scrollSection.children, param);
+            refresh(param);
+            _scroll(scrollSection.querySelectorAll("*>figure.active"), param);
         }
 
     }
@@ -155,24 +209,47 @@ function _update() {
 
 }
 
-function _scrollRefreshData(els, param) {
-    for (var i = 0; i < 10; i++) {
+function _scroll(els, param) {
+    var fillItemsNumber = els.length;
+    for (var i = 0; i < fillItemsNumber; i++) {
         if (param.back) {
             StyleHelpers.applyElementStyle(els[i], {
                 transition: "transform 400ms ease"
             });
         } else {
             StyleHelpers.applyElementStyle(els[i], {
-                transition: ""
+                transition: "transform 100ms ease"
             });
         }
 
-        StyleHelpers.applyTransformStyle(els[i], "translateY(" + param.diff + "px)");
+        StyleHelpers.applyTransformStyle(els[i], "translateY(" + param.offset.y + "px)");
     }
 }
 
 
+function refresh(param) {
+    // var fillItemsNumber = calculate();
+    var scrollSection = document.querySelector(".section");
+    if (isDataThreshold(param, cursor)) {
+        recycle();
+        refreshData();
+        // _scroll(scrollSection.querySelectorAll("*>figure.active"), param);
+    } else {
 
+    }
+
+}
+
+function recycle() {
+    // cursor.current.start=;
+}
+
+
+function refreshData() {
+
+    // cursor.current.end =;
+    loadData(10);
+}
 
 
 
