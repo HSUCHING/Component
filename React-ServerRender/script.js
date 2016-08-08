841f2d77155086ce378cadaa4545038a4f4b2163
Ching Hsu <martin.hsuching@gmail.com>
2016-08-08 18:10:10 +0800

    add React-ServerRender

---
+++ React-ServerRender/script.js
@@ -0,0 +1,21 @@
+/**
+ * Created by chinghsu on 16/8/8.
+ */
+// var ReactDomServer = require('react-dom/server');
+// var listOfItems = <ul className="list-of-items">
+//     <li className="item-1">Item 1</li>
+//     <li className="item-2">Item 2</li>
+//     <li className="item-3">Item 3</li>
+// </ul>;
+//
+// ReactDom.render(listOfItems, document.getElementById('react-application'));
+// [1,2,3].map(n => n + 1);
+// let tool = "babel";
+
+'use strict';
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+const dirs = {
+    src: './src',
+    dest: '/dest'
+}
---
+++ React-ServerRender/test/script-comiled.js
@@ -0,0 +1,30 @@
+/**
+ * Created by chinghsu on 16/8/8.
+ */
+// var ReactDomServer = require('react-dom/server');
+// var listOfItems = <ul className="list-of-items">
+//     <li className="item-1">Item 1</li>
+//     <li className="item-2">Item 2</li>
+//     <li className="item-3">Item 3</li>
+// </ul>;
+//
+// ReactDom.render(listOfItems, document.getElementById('react-application'));
+// [1,2,3].map(n => n + 1);
+// let tool = "babel";
+
+'use strict';
+
+var _gulp = require('gulp');
+
+var _gulp2 = _interopRequireDefault(_gulp);
+
+var _gulpBabel = require('gulp-babel');
+
+var _gulpBabel2 = _interopRequireDefault(_gulpBabel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var dirs = {
+  src: './src',
+  dest: '/dest'
+};
